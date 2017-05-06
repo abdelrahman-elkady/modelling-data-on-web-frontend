8b16f2ca87ce5f91b5d9a44868f725af834dd92b
kady <elkadyabdelrahman@gmail.com>
2017-05-06 23:32:21 +0200

    Basic segments with different cities

---
+++ src/components/Cities.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="ui green centered segment">
+      <h1>Luxor</h1>
+
+    </div>
+    <div class="ui green centered segment">
+      <h1>Hawaii</h1>
+
+    </div>
+    <div class="ui green centered segment">
+      <h1>New York</h1>
+
+    </div>
+    <div class="ui green centered segment">
+      <h1>Dubai</h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cities'
+}
+</script>
+
+<style scoped>
+  .ui.segment {
+    max-width: 50%;
+    margin: 0 auto;
+    margin-bottom: 30px;
+    padding: 20px;
+  }
+</style>
