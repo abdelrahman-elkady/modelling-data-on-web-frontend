48bce8ea443ca9c04240a7ea0448aea490b732c4
kady <elkadyabdelrahman@gmail.com>
2017-05-06 13:32:43 +0200

    Use vue-resource + rename component to home

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 
 import '../node_modules/semantic-ui/dist/semantic.min.css'
 import semantic from 'semantic' // eslint-disable-line
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
